1195bfaf41888ad06863908f83fd15be5adcc082
HudaHamed123 <hodahamed546@gmail.com>
2026-01-07 02:38:21 +0200

    second

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Don't prerender dynamic project detail pages (they require getPrerenderParams)
+  {
+    path: 'projects/:id',
+    renderMode: RenderMode.Client
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
