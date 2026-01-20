b4892bebdcb32b1021c880bffd40edaaa672e0be
HudaHamed123 <hodahamed546@gmail.com>
2026-01-21 00:28:35 +0200

    routing details projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProjectDetailsComponent } from './pages/projects/details/details.component';
-
-
 export const routes: Routes = [
   { path:'', redirectTo:'home', pathMatch:'full' },
   { path:'home', loadComponent:()=>import('./pages/home/home.component').then(m=>m.HomeComponent), title:'home' },
@@ -9,7 +6,7 @@ export const routes: Routes = [
   { path:'contacts', loadComponent:()=>import('./pages/contacts/contacts.component').then(m=>m.ContactsComponent), title:'contacts' },
   { path:'services', loadComponent:()=>import('./pages/services/services.component').then(m=>m.ServicesComponent), title:'services' },
   { path:'projects', loadComponent:()=>import('./pages/projects/projects.component').then(m=>m.ProjectsComponent), title:'projects' },
-    { path: 'projects/:id', component: ProjectDetailsComponent },
+    { path: 'projects/:id', loadComponent:()=>import('./pages/projects/details/details.component').then(m=>m.ProjectDetailsComponent) },
   { path:'**', loadComponent:()=>import('./pages/notfound/notfound.component').then(m=>m.NotfoundComponent), title:'notfound' },
 ];
 
